4bb22d4e80aca2f2d7d65bd55962d63a7651e205
Jonathing <me@jonathing.me>
2025-08-19 21:29:25 -0400

    gradle-3.0 - Split from GradleUtils into dedicated plugins
    Release notes:
    https://gist.github.com/Jonathing/c96051409fcdef58cc51bea8b2b7d4af

--- gitversion-gradle/common/src/main/java/net/minecraftforge/gitversion/gradle/common/GitVersionTools.java
+++ gitversion-gradle/common/src/main/java/net/minecraftforge/gitversion/gradle/common/GitVersionTools.java
@@ -13,9 +13,9 @@ public final class GitVersionTools {
 
     // Git Version
     private static final String GITVERSION_NAME = "gitversion";
-    private static final String GITVERSION_VERSION = "0.7.2";
+    private static final String GITVERSION_VERSION = "0.7.3";
     private static final String GITVERSION_URL = FORGE_MAVEN + "net/minecraftforge/gitversion/" + GITVERSION_VERSION + "/gitversion-" + GITVERSION_VERSION + "-fatjar.jar";
     private static final int GITVERSION_JAVA = 17;
     private static final String GITVERSION_MAIN = "net.minecraftforge.gitver.cli.Main";
-    public static final Tool GITVERSION = Tool.of(GITVERSION_NAME, GITVERSION_NAME, GITVERSION_URL, GITVERSION_JAVA, GITVERSION_MAIN);
+    public static final Tool GITVERSION = Tool.of(GITVERSION_NAME, GITVERSION_VERSION, GITVERSION_URL, GITVERSION_JAVA, GITVERSION_MAIN);
 }
