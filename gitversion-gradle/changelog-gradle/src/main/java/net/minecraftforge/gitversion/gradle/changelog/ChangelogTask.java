2c6f8c0ec8c8d10700ca059ae6ed5f43838080ac
Jonathing <me@jonathing.me>
2025-09-08 19:58:31 -0400

    Fix changelog gradle failing to build

--- gitversion-gradle/changelog-gradle/src/main/java/net/minecraftforge/gitversion/gradle/changelog/ChangelogTask.java
+++ gitversion-gradle/changelog-gradle/src/main/java/net/minecraftforge/gitversion/gradle/changelog/ChangelogTask.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 package net.minecraftforge.gitversion.gradle.changelog;
 
 import net.minecraftforge.gradleutils.shared.EnhancedPlugin;
