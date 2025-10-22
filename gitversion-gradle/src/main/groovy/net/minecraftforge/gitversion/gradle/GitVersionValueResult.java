9a763ef4f5d03ea7a961fd53e2fde05cf53d3964
Jonathing <me@jonathing.me>
2025-10-22 18:49:27 -0400

    Update license of the result record

--- gitversion-gradle/src/main/groovy/net/minecraftforge/gitversion/gradle/GitVersionValueResult.java
+++ gitversion-gradle/src/main/groovy/net/minecraftforge/gitversion/gradle/GitVersionValueResult.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Forge Development LLC
+ * SPDX-License-Identifier: LGPL-2.1-only
+ */
 package net.minecraftforge.gitversion.gradle;
 
 record GitVersionValueResult(GitVersionExtensionInternal.Output output, String errorOutput, Throwable execFailure) { }
